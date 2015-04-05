f09aae80264f34b3cd94ad1f051e13fd1f5c2e45
Ron M Burgess <ronaldmburgess@sbcglobal.net>
2015-04-05 15:42:25 -0400

    Parse Functionality and Scene
    Added parse functionality to the build.
    The two new script create an object in the cloud, and create a user in
    the cloud.
    
    A new Scene has been added that has both two objects, One that links to
    the parse cloud, and another for testing purposes that creates objects
    and users in the cloud.

---
+++ ParsingTextFiles/Assets/Scripts/Parse_Test_Script.cs
@@ -0,0 +1,19 @@
+﻿using Parse;
+using UnityEngine;
+using System.Collections;
+
+public class Parse_Test_Script : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject["foo"] = "bar";
+        testObject.SaveAsync();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ ParsingTextFiles/Assets/Scripts/Parse_Test_User_Script.cs
@@ -0,0 +1,31 @@
+﻿using Parse;
+using UnityEngine;
+using System.Collections;
+using System.Threading.Tasks;
+
+public class Parse_Test_User_Script : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Debug.Log("Creating new User Steve");
+        var user = new ParseUser()
+        {
+            Username = "Steve Johnson",
+            Password = "SteveJohnsonIsThebomb2022",
+            Email = "SteveJohnsonRules@gmail.com"
+            
+        };
+        Debug.Log("Created new user Steve");
+        // other fields can be set just like with ParseObject
+        //user["phone"] = "650-555-0000";
+
+        Task signUpTask = user.SignUpAsync();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+}
